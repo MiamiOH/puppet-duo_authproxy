08094ac2c121f9a15bad1364803cf22174cc8cf4
jmcnatt <jimmy@jmcnatt.net>
2020-07-30 18:10:31 -0700

    Added hiera lookup with merge logic

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,19 +1,25 @@
-# duo_authproxy::config
+# @summary Configures the Duo Authentication Proxy by setting the config file.
 #
-# Private class that configures the auth proxy
+# @api private
 #
-# @summary configure the proxy
-#
-# @example
-#   don't use this class directly
 class duo_authproxy::config {
 
-  file { 'authproxy.cfg':
+  $config_file = 'authproxy.cfg'
+
+  # Determine if settings are available in hiera and apply them
+  $hiera_settings = lookup('duo_authproxy::settings', { merge => 'deep' })
+  $settings = $hiera_settings ? {
+    undef   => $duo_authproxy::settings,
+    default => $hiera_settings,
+  }
+
+  file { $config_file:
     ensure  => file,
-    path    => "${duo_authproxy::install_dir}/conf/authproxy.cfg",
+    path    => "${duo_authproxy::install_dir}/conf/${config_file}",
     owner   => 'nobody',
     group   => 'root',
     mode    => '0400',
-    content => Sensitive(template("${module_name}/authproxy.cfg.erb")),
+    content => Sensitive(template("${module_name}/${config_file}.erb")),
   }
+
 }
